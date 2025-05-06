7da9e69c60050c750353bbb58f66bc980942442a
joaquingrimoldi <41316054-joaquingrimoldi@users.noreply.replit.com>
2025-05-06 01:14:41 +0000

    Restored to '9b799b2e17776d621d2657219e0dca6d97dcddf3'
    Replit-Restored-To: 9b799b2e17776d621d2657219e0dca6d97dcddf3

--- client/src/components/ImageCard.tsx
+++ client/src/components/ImageCard.tsx
@@ -9,9 +9,9 @@ import {
   TrashIcon,
   RotateCcwIcon,
   CopyIcon,
-  ImageIcon
+  ImageIcon,
+  ZoomInIcon
 } from "lucide-react";
-import ImageUpscale from "./icons/ImageUpscale";
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { GeneratedImage } from "@/types/image";
@@ -266,7 +266,7 @@ export default function ImageCard({
                       onUpscale(image);
                     }}
                   >
-                    <ImageUpscale className="h-4 w-4" />
+                    <ZoomInIcon className="h-4 w-4" />
                   </Button>
                 </TooltipTrigger>
                 <TooltipContent>Upscale</TooltipContent>
