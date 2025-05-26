3e0043e3a5ba2435cc6d1287d90df4a6f11cf1a0
joaquingrimoldi <41316054-joaquingrimoldi@users.noreply.replit.com>
2025-05-26 22:23:03 +0000

    Restored to '5eacb53e8fa826dea2aadf946bc42a6087a5bea9'
    Replit-Restored-To: 5eacb53e8fa826dea2aadf946bc42a6087a5bea9

--- client/src/components/email-builder/index.tsx
+++
@@ -1,8 +0,0 @@
-// Export all email builder components
-export { EmailBuilderProvider, useEmailBuilder } from './EmailBuilderContext';
-export { TemplateSelector } from './TemplateSelector';
-export { ComponentSidebar } from './ComponentSidebar';
-export { EmailCanvas } from './EmailCanvas';
-export { PropertiesPanel } from './PropertiesPanel';
-export { PreviewPane } from './PreviewPane';
-export { ImageGalleryModal } from './ImageGalleryModal';
